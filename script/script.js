7c7a13cdd4e71dad75ef28e66e2971b8c72adff4
alamtalukder <shahalamcse7@gmail.com>
2026-02-24 23:55:16 +0600

    done

--- script/script.js
+++ script/script.js
@@ -1,5 +1,7 @@
 let interviewList = []; 
 let rejectedList = [];
+let currentStatus = "All";
+
 let totalJobs = document.getElementById("total");
 let totalInterviews = document.getElementById("interviewCount");
 let totalRejected = document.getElementById("RejectedCount");
@@ -25,6 +27,7 @@ calculateCount();
 
 function toggleStyle(buttonId){
     const buttons = [allFilterBtn, interviewFilterBtn, rejectedFilterBtn];
+    currentStatus = buttonId; 
     buttons.forEach(button => {
         if(button.id === buttonId){
             button.classList.add("bg-blue-500", "text-white");
@@ -34,14 +37,26 @@ function toggleStyle(buttonId){
             button.classList.add("bg-gray-300");
         }
     });
+    if(buttonId === "all-filter-btn"){
+        filteredSection.classList.add("hidden");
+        allJobsCard.classList.remove("hidden");
+    } else if(buttonId === "interview-btn"){
+        filteredSection.classList.remove("hidden");
+        allJobsCard.classList.add("hidden");
+    } else if(buttonId === "rejected-btn"){
+        filteredSection.classList.remove("hidden");
+        allJobsCard.classList.add("hidden");
+    }
 }
 mainContainer.addEventListener("click", function(event){
+    if(event.target.classList.contains("interview-btn")){
     const parentNode = event.target.parentNode.parentNode;
     const jobTitle = parentNode.querySelector("h1").innerText;
     const jobPosition = parentNode.querySelector("p:nth-child(2)").innerText;
     const salary = parentNode.querySelector("p:nth-child(3)").innerText;
     const statusElement = parentNode.querySelector("p.status");
     const discription = parentNode.querySelector("p.discription").innerText;
+    parentNode.querySelector("p.status").innerText = "Interview";
     const jobData = {
         title: jobTitle,
         position: jobPosition,
@@ -50,17 +65,49 @@ mainContainer.addEventListener("click", function(event){
         discription: discription
     }
     const existingJob = interviewList.find(item => item.title == jobData.title); 
+   
     if (!existingJob) {
         interviewList.push(jobData);
     }
-    renderInterviewList()   
+    interviewList = interviewList.filter(item => item.title != jobData.title);
+    if(currentStatus === "interview-btn"){
+        filteredSection.innerHTML = "";
+        renderInterviewList() 
+    }
+    calculateCount();
+} else if(event.target.classList.contains("rejected-btn")){
+    const parentNode = event.target.parentNode.parentNode;
+    const jobTitle = parentNode.querySelector("h1").innerText;
+    const jobPosition = parentNode.querySelector("p:nth-child(2)").innerText;
+    const salary = parentNode.querySelector("p:nth-child(3)").innerText;
+    const statusElement = parentNode.querySelector("p.status");
+    const discription = parentNode.querySelector("p.discription").innerText;
+    parentNode.querySelector("p.status").innerText = "Rejected";
+    const jobData = {
+        title: jobTitle,
+        position: jobPosition,
+        salary: salary,
+        status: statusElement.innerText,
+        discription: discription
+    }
+    const existingJob = rejectedList.find(item => item.title == jobData.title); 
+   
+    if (!existingJob) {
+        rejectedList.push(jobData);
+    }
+    rejectedList = rejectedList.filter(item => item.title != jobData.title);
+    if(currentStatus === "rejected-btn"){
+        filteredSection.innerHTML = ""; 
+        renderRejectedList()
+    }
+    calculateCount();
 }
-)
 function renderInterviewList(){
     filteredSection.innerHTML = "";
     for(let job of interviewList){
+        console.log(job);
         const jobCard = document.createElement("div"); 
-        jobCard.classList.add("flex", "justify-between", "border", "p-10", "mb-5");
+        jobCard.className = "card flex justify-between border p-10 mb-5 mx-auto w-[80%]";
         jobCard.innerHTML = `
             <div class="space-y-4">
                 <h1 class="job-title">${job.title}</h1>
@@ -76,4 +123,26 @@ function renderInterviewList(){
         filteredSection.appendChild(jobCard);
     }
 }
+function renderRejectedList(){
+    filteredSection.innerHTML = "";
     
+    for(let job of rejectedList){
+        console.log(job);
+        const jobCard = document.createElement("div"); 
+        jobCard.className = "card flex justify-between border p-10 mb-5 mx-auto w-[80%]";
+        jobCard.innerHTML = `
+            <div class="space-y-4">
+                <h1 class="job-title">${job.title}</h1>
+                <p class="job-position">${job.position}</p>
+                <p class="salary">${job.salary}</p>
+                <p class="text-red-500 status">${job.status}</p>
+                <p class="discription">${job.discription}</p>   
+            </div>
+            <div class="flex gap-2">
+                <button class="rejected-btn text-red-400">REJECTED</button>
+            </div>
+        `
+        filteredSection.appendChild(jobCard);
+    }
+}
+}) 
